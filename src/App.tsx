071248402e77863d688ddecb5a5f15fd7c51f9b8
chidiebereojingwa <ojingwaoji@gmail.com>
2022-07-04 15:37:08 +0100

    updated app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,19 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+  const startTrivia =async () => {
+    
+  }
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const nextQuestion = () => {
+
+  }
+  
   return (
     <div className="App">
       <h1> Hello world</h1>
